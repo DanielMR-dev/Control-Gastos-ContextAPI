3a9f0540511fc31be9cd68f5b9143234a3d87727
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-09-28 16:07:00 -0500

    194. Creando el Reducer para Presupuesto

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,31 @@
+
+// Creacion del type para las acciones del Budget
+export type BudgetActions =
+    { type: 'add-budget', payload: {budget: number} }
+
+// Creacion del type del State para el Budget     
+export type BudgetState = {
+    budget: number
+}
+
+// Creación del estado inicial
+export const initialState : BudgetState = {
+    budget: 0
+}
+
+// Creacion del reducer para el Budget
+export const budgetReducer = (
+        state: BudgetState = initialState,
+        actions: BudgetActions
+    ) => {
+    if(actions.type === 'add-budget') {
+        return {
+            ...state,
+            budget: actions.payload.budget
+        }
+    }
+
+
+    return state
+
+}
