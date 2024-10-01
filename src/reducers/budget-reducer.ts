5ed95078b1261c09d088c0b565bc05644d0aa504
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-09-30 21:41:43 -0500

    216. Eliminando un gasto cuando ejecutamos un Swipe

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,7 +6,8 @@ export type BudgetActions =
     { type: 'add-budget', payload: {budget: number} } |
     { type: 'show-modal' } |
     { type: 'close-modal' } |
-    { type: 'add-expense', payload: {expense: DraftExpense}}
+    { type: 'add-expense', payload: {expense: DraftExpense} } |
+    { type: 'remove-expense', payload: {id: Expense['id']} }
 
 // Creacion del type del State para el Budget     
 export type BudgetState = {
@@ -64,6 +65,13 @@ export const budgetReducer = (
             ...state,
             expenses: [...state.expenses, expense],
             modal: false
+        }
+    }
+
+    if(action.type === 'remove-expense') {
+        return {
+            ...state,
+            expense: state.expenses.filter(expense => expense.id !== action.payload.id)
         }
     }
 
